f6db48d22eff4ffb97f03756079e7b80695aa829
minghunt <114232628+minghunt@users.noreply.github.com>
2023-04-06 01:22:05 +0700

    add slider

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -1,17 +1,68 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./Banner.css";
-import AwesomeSlider from 'react-awesome-slider';
+import Carousel from 'react-bootstrap/Carousel';
 import slider_1 from "./slider_1.webp"
-import 'react-awesome-slider/dist/styles.css';
-const Banner = ()=>{
+import slider_2 from "./slider_2.webp"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {FreeMode} from 'swiper';
+import 'swiper/css';
+import "swiper/css/free-mode";
+const Banner = () => {
+    const [index, setIndex] = useState(0);
 
-    return (<div>
-        <AwesomeSlider
-  >
-    <div data-src={slider_1} />
-    <div data-src={slider_1} />
-   
-  </AwesomeSlider>
-    </div>)
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+    return (
+        <div className='body'>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider_1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider_1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider_1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+            <Swiper freeMode={true}
+            grabCursor={true}
+            modules={[FreeMode]}
+            className='mySwiper'
+            slidesPerView={4}
+            spaceBetween={15}
+            >
+            <SwiperSlide className='sl'>
+                <h4>GIẢNG VIÊN UY TÍN</h4>
+                <p>Bài học chất lượng</p>
+            </SwiperSlide>
+            <SwiperSlide className='sl'>
+                <h4>GIẢNG VIÊN UY TÍN</h4>
+                <p>Bài học chất lượng</p>
+            </SwiperSlide><SwiperSlide className='sl'>
+                <h4>GIẢNG VIÊN UY TÍN</h4>
+                <p>Bài học chất lượng</p>
+            </SwiperSlide><SwiperSlide className='sl'>
+                <h4>GIẢNG VIÊN UY TÍN</h4>
+                <p>Bài học chất lượng</p>
+            </SwiperSlide>
+            </Swiper>
+        </div>
+
+    )
 }
 export default Banner;
