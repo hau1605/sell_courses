32d7989b3fab62aa80d176e138fbb3d8922ac811
minghunt <114232628+minghunt@users.noreply.github.com>
2023-04-06 21:35:32 +0700

    add service ads

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -10,82 +10,92 @@ import service_2 from "./service_2.png"
 import service_3 from "./service_3.png"
 import service_4 from "./service_4.webp"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination} from 'swiper';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper';
 import { FreeMode } from 'swiper';
 import 'swiper/css';
 import "swiper/css/free-mode";
 import 'swiper/css/navigation';
+import Button from 'react-bootstrap/Button';
 const Banner = () => {
     const [index, setIndex] = useState(0);
-
+    const swiper = useSwiper();
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
     return (
         <div className='body'>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src='https://bizweb.dktcdn.net/thumb/2048x2048/100/453/393/themes/894913/assets/slider_1.png?1676281841878'
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src='https://bizweb.dktcdn.net/thumb/2048x2048/100/453/393/themes/894913/assets/slider_1.png?1676281841878'
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src='https://bizweb.dktcdn.net/thumb/2048x2048/100/453/393/themes/894913/assets/slider_1.png?1676281841878'
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-            </Carousel>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                grabCursor={true}
+                navigation={true}
+                modules={[Autoplay, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img
+                    className="d-block w-100"
+                    src='https://bizweb.dktcdn.net/thumb/2048x2048/100/453/393/themes/894913/assets/slider_1.png?1676281841878'
+                    alt="First slide"
+                /></SwiperSlide>
+                <SwiperSlide><img
+                    className="d-block w-100"
+                    src='https://bizweb.dktcdn.net/thumb/2048x2048/100/453/393/themes/894913/assets/slider_1.png?1676281841878'
+                    alt="First slide"
+                /></SwiperSlide>
+                <SwiperSlide><img
+                    className="d-block w-100"
+                    src='https://bizweb.dktcdn.net/thumb/2048x2048/100/453/393/themes/894913/assets/slider_1.png?1676281841878'
+                    alt="First slide"
+                /></SwiperSlide>
+            </Swiper>
+
+
             <div className='service'>
-                <Swiper 
-    modules={[Navigation, Pagination]}
-                    navigation={true} 
+                <Swiper
                     loop={true}
-                    className='mySwiper'          
+
+                    modules={[Navigation]}
+                    navigation={true}
+                    className='mySwiper'
                     breakpoints={
                         {
-                            0:{
-                                slidesPerView:1,
-                                spaceBetween:10
-                            },530:{
-                                slidesPerView:2,
-                                spaceBetween:10
-                            },799:{
-                                slidesPerView:3,
-                                spaceBetween:15
-                            },1075:{
-                                slidesPerView:4,
-                                spaceBetween:20
+                            0: {
+                                slidesPerView: 1,
+                                spaceBetween: 10
+                            }, 530: {
+                                slidesPerView: 2,
+                                spaceBetween: 10
+                            }, 799: {
+                                slidesPerView: 3,
+                                spaceBetween: 15
+                            }, 1075: {
+                                slidesPerView: 4,
+                                spaceBetween: 20
                             }
                         }
                     }
                 >
                     <SwiperSlide>
                         <Container>
-                        <Row className='row'>
-                            <Col className='col-img' xs='3'>
-                                <img className='icon' src={service_1} />
-                            </Col>
-                            <Col className='col-text'>
-                                <Row>
-                                    <p className='top-text'>GIẢNG VIÊN UY TÍN</p>
-                                </Row>
-                                <Row>
-                                    <p className='bot-text'>Bài học chất lượng</p>
-                                </Row>
-                            </Col>
-                        </Row>
+                            <Row className='row'>
+                                <Col className='col-img' xs='3'>
+                                    <img className='icon' src={service_1} />
+                                </Col>
+                                <Col className='col-text'>
+                                    <Row>
+                                        <p className='top-text'>GIẢNG VIÊN UY TÍN</p>
+                                    </Row>
+                                    <Row>
+                                        <p className='bot-text'>Bài học chất lượng</p>
+                                    </Row>
+                                </Col>
+                            </Row>
                         </Container>
                     </SwiperSlide>
                     <SwiperSlide>
