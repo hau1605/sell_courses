de9d5b778fea7a674d9b9f1383b588ffb8b24d0c
minghunt <114232628+minghunt@users.noreply.github.com>
2023-04-09 22:30:33 +0700

    them homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 
+import { useEffect } from 'react';
 import './App.css';
 import WebLayout from './components/Layout';
 
 function App() {
+  useEffect(()=>{window.scrollTo(0, 0);},[])
   return (
     <div className="App">
       <WebLayout />
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,16 +2,19 @@ import React from 'react';
 import logo from "./logo.webp"
 import "./Header.css"
 import { Col, Row } from 'antd';
+import { Link } from 'react-router-dom';
 const Header = () => (
 
       <div className='header'>
         <div className='header_left'>
-            <img className='icon' src={logo} />      
+            <Link to='/'><img className='icon' src={logo} />  </Link>    
         </div>
         <div className='header_center'>
           <ul className='header_menu'>
             <div className='dropDown_header'>
+                <Link to='/ProductList'>
                 <button className='dropDown_button'>Danh mục</button>
+                </Link>
                   <div className='dropDown_list'>
                     <a href='#'>Trang chủ</a>
                     <a href='#'>Danh sách khóa học</a>
