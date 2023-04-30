9f6df1cdc173c57997f78c41f5540cfcb703f08a
Phuc Hoang <71083508+mcbutt007@users.noreply.github.com>
2023-04-30 16:35:11 +0700

    complete admin page (#12)
    * make importing routes cleaner
    
    * added resources to admin page
    
    * add authentication to admin page

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,38 @@ import AdminJSExpress from "@adminjs/express";
 import express from "express";
 import { connectDB } from "./db/connection.js";
 import cors from "cors";
-
-
-import coursesRoutes from './routes/coursesRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
-import categoriesRoutes from './routes/categoriesRoutes.js';
-import topicsRoutes from './routes/topicsRoutes.js';
-import usersRoutes from './routes/usersRoutes.js';
-import loginRoutes from './routes/loginRoutes.js';
-import lecturersRoutes from './routes/lecturersRoutes.js';
-import topWeeksRoutes from './routes/topWeeksRoutes.js';
-import facebookUsersRoutes from './routes/facebookUsersRoutes.js';
-
+import {
+  coursesRoutes,
+  cartRoutes,
+  categoriesRoutes,
+  topicsRoutes,
+  usersRoutes,
+  loginRoutes,
+  lecturersRoutes,
+  topWeeksRoutes,
+  facebookUsersRoutes,
+} from "./routes/index.js";
+import * as models from "./models/index.js";
 import { PORT } from "./config/config.js";
 import * as config from "./config/config.js";
+import * as AdminJSMongoose from "@adminjs/mongoose";
+
+AdminJS.registerAdapter({
+  Resource: AdminJSMongoose.Resource,
+  Database: AdminJSMongoose.Database,
+});
+
+const DEFAULT_ADMIN = {
+  email: "admin@example.com",
+  password: "password",
+};
+
+const authenticate = async (email, password) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
 
 const start = async () => {
   const app = express();
@@ -26,9 +44,42 @@ const start = async () => {
   app.use(express.json());
   app.use(cors());
 
-  const admin = new AdminJS({});
+  // Admin setup
+  const adminOptions = {
+    resources: [
+      models.courseModel,
+      models.cartModel,
+      models.categoryModel,
+      models.userModel,
+      models.lecturerModel,
+      models.topicModel,
+      models.topWeekModel,
+      models.facebookUserModel,
+    ],
+  };
 
-  const adminRouter = AdminJSExpress.buildRouter(admin);
+  const admin = new AdminJS(adminOptions);
+
+  const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    admin,
+    {
+      authenticate,
+      cookieName: "adminjs",
+      cookiePassword: "sessionsecret",
+    },
+    null,
+    {
+      resave: true,
+      saveUninitialized: true,
+      secret: "sessionsecret",
+      cookie: {
+        httpOnly: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production",
+      },
+      name: "adminjs",
+    }
+  );
+
   app.use(admin.options.rootPath, adminRouter);
 
   // Routes
@@ -47,6 +98,7 @@ const start = async () => {
     console.log(`Server is running on port ${config.PORT}`);
     console.log(
       `AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`
+	    + "  email: 'admin@example.com',password: 'password',"
     );
   });
 };
