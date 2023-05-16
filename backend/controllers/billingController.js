c2f1df0b389bfb54e1b9b4b507cead60d141001b
Phuc Hoang <hoangmaithienphuc@gmail.com>
2023-05-16 22:50:16 +0700

    update

--- backend/controllers/billingController.js
+++ backend/controllers/billingController.js
@@ -1,5 +1,6 @@
 import * as billingDAO from "../dao/billingsDAO.js";
 import { getUserById } from "../dao/usersDAO.js";
+import mongoose from "mongoose";
 
 // GET /api/billings
 async function getBillings(req, res) {
@@ -68,32 +69,32 @@ async function deleteBillingById(req, res) {
 }
 
 async function purchase(req, res) {
-  const { user_id, orders } = req.body;
-  // Check if user_id exists in the billing collection -> update orders array
-  userExists(user_id)
+  const { user_id, user_name, email, orders } = req.body;
+  // Check if user_id exists in the billing collection 
+  billingDAO.userExists(user_id)
     .then((exists) => {
       if (exists) {
-        // User exists in billing collection;
+        // User exists in billing collection -> update orders array
+	console.log("User exists in billing collection");
       } else {
         // User does not exist in billing collection -> create a new billing document
-        const userdata = getUserById(user_id);
         const billingData = {
-          user_id: mongoose.Types.ObjectId(user_id),
-          user_name: userdata.name,
-          email: userdata.email,
+          user_id: user_id,
+          user_name: user_name,
+          email: email,
           orders: [],
         };
         // Insert orders
         let total_amount = 0;
         let order = {
-          order_id: new ObjectId(),
+          order_id: new mongoose.Types.ObjectId(),
           payment_method: {
             type: orders.payment_method.type,
             card_number: orders.payment_method.card_number,
             expiration_date: orders.payment_method.expiration_date,
             security_code: orders.payment_method.security_code,
           },
-          date: orders.date,
+          date: Date.now(),
           status: "pending",
           items: [],
           total_amount: 0,
@@ -122,19 +123,9 @@ async function purchase(req, res) {
       console.error("Failed to check user existence:", error);
     });
 
-  try {
-    // Create a new Billing object
-    const billing = new billing({
-      user_id: mongoose.Types.ObjectId(user_id),
-      items: items.map((item) => ({
-        course_id: mongoose.Types.ObjectId(item.course_id),
-        course_name: item.course_name,
-        course_price: item.course_price,
-      })),
-    });
-
     // Save the Billing object to the database
-    await billing.save();
+  try {
+    // await billing.save();
 
     res.status(201).json({ message: "Purchase successful" });
   } catch (error) {
