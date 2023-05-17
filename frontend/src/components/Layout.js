35cf88332a3073633f3c8f3c59c214b1cd5a3fb5
hau1605 <phmh165@gmail.com>
2023-05-17 14:49:32 +0700

    redirect, upt header

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Card, Layout, Space } from 'antd';
-import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Space } from 'antd';
+import { Routes, Route, Navigate,  } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import Header from './Header/Header';
 import HomePage from "./HomePage/HomePage"
-import Banner from './Banner/Banner';
 import ProductDetail from './ProductDetail/ProductDetail';
 import Login from './Login/Login';
 import Register from './Register/Register';
@@ -13,53 +13,56 @@ import Cart from './Cart/Cart';
 import Contact from './Contact/Contact';
 import ResetPass from './ResetPass/ResetPass';
 import MyProducts from './User/MyProducts/MyProducts';
-import User from './User/User'
 import UserAccount from "./User/UserAccount";
 import UserPassword from "./User/UserPassword";
 import { SnackbarProvider } from 'notistack';
 import Footer from './Footer/Footer';
 import VideoPage from './Video/VideoPage';
-const WebLayout = () => (
-  <SnackbarProvider maxSnack={4} autoHideDuration={1500}
-  iconVariant={{
-    success: '🛒',
-    error: '🗑️',
-    warning: '⚠️',
-    info: 'ℹ️',
-  }}
-  >
-    <Space
-      direction="vertical"
-      style={{
-        width: '100%',
-      }}
-      size={[0, 48]}
+const WebLayout = () => {
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
+  return(
+    <SnackbarProvider maxSnack={4} autoHideDuration={1500}
+    iconVariant={{
+      success: '🛒',
+      error: '🗑️',
+      warning: '⚠️',
+      info: 'ℹ️',
+    }}
     >
-      <Layout>
-        <Header style={{ zIndex: 1 }} >Header</Header>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/ProductDetail" element={<ProductDetail />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path='/Contact' element={<Contact />} />
-          <Route path="/ProductList/:categorie/" element={<ProductList />} />
-          <Route path='/order' element={<CheckOut />} />
-          <Route path="/ProductList/" element={<ProductList />} />
-          <Route path="/account/login" element={<Login />} />
-          <Route path="/account/register" element={<Register />} />
-          <Route path="/account/reset-password" element={<ResetPass />} />
-          <Route path="/user" element={<UserAccount />} />
-          <Route path="/user/profile" element={<UserAccount/>} />
-          <Route path="/user/password" element={<UserPassword/>} />
-          <Route path="/user/my-course" element={<MyProducts/>} />
-          <Route path="/user/my-course/:id" element={<VideoPage/>} />
-        </Routes>
-        <Footer/>
-      </Layout>
-
-    </Space>
-  </SnackbarProvider>
-
-
-);
+      <Space
+        direction="vertical"
+        style={{
+          width: '100%',
+        }}
+        size={[0, 48]}
+      >
+        <Layout style={{
+          height: '100vh',
+          margin: '0',
+        }}>
+          <Header style={{ zIndex: 1 }} >Header</Header>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/ProductDetail" element={<ProductDetail />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path='/Contact' element={<Contact />} />
+            <Route path="/ProductList/:categorie/" element={<ProductList />} />
+            <Route path='/order' element={!isLoggedIn? <Navigate to={'/account/login'} replace /> : <CheckOut />} />
+            <Route path="/ProductList/" element={<ProductList />} />
+            <Route path="/account/login" element={isLoggedIn? <Navigate to={'/'} replace /> : <Login />} />
+            <Route path="/account/register" element={isLoggedIn? <Navigate to={'/'} replace /> : <Register />} />
+            <Route path="/account/reset-password" element={isLoggedIn? <Navigate to={'/'} replace /> : <ResetPass />} />
+            <Route path="/user" element={!isLoggedIn? <Navigate to={'/account/login'} replace /> : <UserAccount />} />
+            <Route path="/user/profile" element={!isLoggedIn? <Navigate to={'/account/login'} replace /> : <UserAccount/>} />
+            <Route path="/user/password" element={!isLoggedIn? <Navigate to={'/account/login'} replace /> : <UserPassword/>} />
+            <Route path="/user/my-course" element={!isLoggedIn? <Navigate to={'/account/login'} replace /> : <MyProducts/>} />
+            <Route path="/user/my-course/:id" element={!isLoggedIn? <Navigate to={'/account/login'} replace /> : <VideoPage/>} />
+          </Routes>
+          <Footer/>
+        </Layout>
+      </Space>
+    </SnackbarProvider>
+  )
+};
 export default WebLayout;
