951e7c4be3ab41915b7a9f1ddbf4d55429118085
Phuc Hoang <71083508+mcbutt007@users.noreply.github.com>
2023-05-18 13:13:58 +0700

    /api/billings/purchase now partially works (still missing already (#19)
    purchased course constraint check and does not update owned course in user collection.)

--- backend/controllers/billingController.js
+++ backend/controllers/billingController.js
@@ -67,70 +67,80 @@ async function deleteBillingById(req, res) {
   });
 }
 
+function createOrder(orders) {
+  let total_amount = 0;
+  const order = {
+    order_id: new mongoose.Types.ObjectId(),
+    payment_method: {
+      type: orders.payment_method.type,
+      card_number: orders.payment_method.card_number,
+      expiration_date: orders.payment_method.expiration_date,
+      security_code: orders.payment_method.security_code,
+    },
+    date: Date.now(),
+    status: "pending",
+    items: [],
+    total_amount: 0,
+  };
+
+  for (let j = 0; j < orders.items.length; j++) {
+    const item = {
+      course_id: orders.items[j].course_id,
+      course_name: orders.items[j].course_name,
+      course_price: orders.items[j].course_price,
+    };
+    total_amount += orders.items[j].course_price;
+    order.items.push(item);
+  }
+  order.total_amount = total_amount;
+
+  return order;
+}
+
+async function createBillingDocument(user_id, user_name, email, orders) {
+  const billingData = {
+    user_id: user_id,
+    user_name: user_name,
+    email: email,
+    orders: [],
+  };
+
+  const order = createOrder(orders);
+  billingData.orders.push(order);
+
+  await billingDAO.createBilling(billingData, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+  });
+}
+
+async function updateBillingDocument(user_id, orders) {
+  const userBillingDoc = await billingDAO.findBillingDocumentByUserId(user_id);
+
+  const order = createOrder(orders);
+  userBillingDoc.orders.push(order);
+  const id = { user_id: userBillingDoc.user_id };
+
+  await billingDAO.updateById(id, userBillingDoc, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+  });
+}
+
 async function purchase(req, res) {
   const { user_id, user_name, email, orders } = req.body;
-  // Check if user_id exists in the billing collection 
-  billingDAO.userExists(user_id)
-    .then((exists) => {
-      if (exists) {
-        // User exists in billing collection -> update orders array
-	console.log("User exists in billing collection");
-      } else {
-        // User does not exist in billing collection -> create a new billing document
-        const billingData = {
-          user_id: user_id,
-          user_name: user_name,
-          email: email,
-          orders: [],
-        };
-        // Insert orders
-        let total_amount = 0;
-        let order = {
-          order_id: new mongoose.Types.ObjectId(),
-          payment_method: {
-            type: orders.payment_method.type,
-            card_number: orders.payment_method.card_number,
-            expiration_date: orders.payment_method.expiration_date,
-            security_code: orders.payment_method.security_code,
-          },
-          date: Date.now(),
-          status: "pending",
-          items: [],
-          total_amount: 0,
-        };
-        // Insert items for the order
-        for (let j = 0; j < orders.items.length; j++) {
-          const item = {
-            course_id: orders.items[j].course_id,
-            course_name: orders.items[j].course_name,
-            course_price: orders.items[j].course_price,
-          };
-          total_amount += orders.items[j].course_price;
-          order.items.push(item);
-        }
-        order.total_amount = total_amount;
-
-        billingData.orders.push(order);
-        billingDAO.createBilling(billingData, (err) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-        });
-      }
-    })
-    .catch((error) => {
-      console.error("Failed to check user existence:", error);
-    });
-
-    // Save the Billing object to the database
-  try {
-    // await billing.save();
-
-    res.status(201).json({ message: "Purchase successful" });
-  } catch (error) {
-    console.error("Failed to complete purchase:", error);
-    res.status(500).json({ error: "Failed to complete purchase" });
+  // Check if user_id exists in the billing collection
+  const exists = await billingDAO.userExists(user_id);
+  if (exists) {
+    // User exists in billing collection -> update orders array
+    updateBillingDocument(user_id, orders);
+  } else {
+    // User does not exist in billing collection -> create a new billing document
+    createBillingDocument(user_id, user_name, email, orders);
   }
+  return res.status(200).send({ message: "Purchase Success" });
 }
 
 export {
--- backend/dao/billingsDAO.js
+++ backend/dao/billingsDAO.js
@@ -3,44 +3,60 @@ import Billing from "../models/billingModel.js";
 
 async function findAll() {
   return Billing.find();
-};
+}
 
 async function createBilling(newBilling) {
-  const billing = new Billing(newBilling);
-  return billing.save();
-};
+  try {
+    const billing = new Billing(newBilling);
+    return await billing.save();
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function findById(id) {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return Promise.reject(`Invalid ID: ${id}`);
   }
   return Billing.findById(id);
-};
+}
 
-async function updateById(id, updateData) {
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return Promise.reject(`Invalid ID: ${id}`);
+async function findBillingDocumentByUserId(user_id) {
+  try {
+    const existingBilling = await Billing.findOne({ user_id: user_id });
+    return existingBilling;
+  } catch (error) {
+    throw error;
   }
-  return Billing.findByIdAndUpdate(id, updateData, { new: true });
-};
+}
+
+async function updateById(filter, updateData) {
+  return Billing.findOneAndUpdate(filter, updateData);
+}
 
 async function deleteById(id) {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return Promise.reject(`Invalid ID: ${id}`);
   }
   return Billing.findByIdAndDelete(id);
-};
+}
 
-async function userExists(userId){
+async function userExists(userId) {
   try {
     const existingBilling = await Billing.findOne({ user_id: userId });
     return !!existingBilling; // Returns true if the billing document exists, false otherwise
   } catch (error) {
-    console.error('Failed to check user existence:', error);
+    console.error("Failed to check user existence:", error);
     throw error;
   }
-};
+}
 
 export {
-  findAll,createBilling,findById,updateById,deleteById,userExists
-}
+  findAll,
+  createBilling,
+  findById,
+  updateById,
+  deleteById,
+  userExists,
+  findBillingDocumentByUserId,
+};
